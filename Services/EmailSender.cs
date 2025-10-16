271e8fc52a8a0e97753d94b405e9c51deba7fe55
hunghoclopa99999 <hunghoclopa99999@gmail.com>
2025-10-16 22:11:52 +0700

    Update EmailSender.cs

---
+++ Services/EmailSender.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace POETWeb.Services
+{
+    public class EmailSettings
+    {
+        public string FromName { get; set; }
+        public string FromEmail { get; set; }
+        public string SmtpServer { get; set; }
+        public int SmtpPort { get; set; }
+        public string SmtpUsername { get; set; }
+        public string SmtpPassword { get; set; }
+    }
+
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailSender(IOptions<EmailSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            using var client = new SmtpClient(_settings.SmtpServer, _settings.SmtpPort)
+            {
+                Credentials = new NetworkCredential(_settings.SmtpUsername, _settings.SmtpPassword),
+                EnableSsl = true
+            };
+
+            var msg = new MailMessage
+            {
+                From = new MailAddress(_settings.FromEmail, _settings.FromName),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+            msg.To.Add(email);
+
+            await client.SendMailAsync(msg);
+        }
+    }
+}
