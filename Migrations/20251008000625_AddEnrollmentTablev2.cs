5c52d0c313903ee6e750221f5c7fa159d4875c33
luongminhh1915 <luonghuuminhlc@gmail.com>
2025-10-17 22:04:26 +0700

    add Migrations

---
+++ Migrations/20251008000625_AddEnrollmentTablev2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POETWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnrollmentTablev2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollment",
+                newName: "Enrollments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollment_ClassId_UserId",
+                table: "Enrollments",
+                newName: "IX_Enrollments_ClassId_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Enrollments",
+                table: "Enrollments");
+
+            migrationBuilder.RenameTable(
+                name: "Enrollments",
+                newName: "Enrollment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Enrollments_ClassId_UserId",
+                table: "Enrollment",
+                newName: "IX_Enrollment_ClassId_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Enrollment",
+                table: "Enrollment",
+                column: "Id");
+        }
+    }
+}
