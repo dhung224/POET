9b3ec773da8855caa4ea082c973ca217cadd4781
Malice <tuananhtrinh29823@gmail.com>
2025-10-17 21:49:13 +0700

    Add Migrations

---
+++ Migrations/20251007165520_AddUniqueIndexOnAccountCode.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace POETWeb.Migrations
+{
+    public partial class AddUniqueIndexOnAccountCode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountCode",
+                table: "AspNetUsers",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AccountCode",
+                table: "AspNetUsers",
+                column: "AccountCode",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AccountCode",
+                table: "AspNetUsers");
+
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AccountCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+
+        }
+    }
+}
