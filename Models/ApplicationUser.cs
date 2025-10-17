8a2935f69e119a91597b1518f93adcb8888fa405
Malice <tuananhtrinh29823@gmail.com>
2025-10-17 21:15:00 +0700

    Update Register

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Identity;
+
+namespace POETWeb.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(80, MinimumLength = 2)]
+        public string FullName { get; set; } = string.Empty;
+
+        // 4 chữ cái + 4 số, ví dụ: ABCD1234
+        [Required]
+        [RegularExpression("^[A-Za-z]{4}\\d{4}$",
+            ErrorMessage = "AccountCode phải gồm 4 chữ cái và 4 số (VD: ABCD1234).")]
+        [StringLength(8)]
+        public string AccountCode { get; set; } = string.Empty;
+
+        [Url]
+        [StringLength(2048)]
+        public string? AvatarUrl { get; set; }
+    }
+}
