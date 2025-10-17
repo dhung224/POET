b1abf6a02d60dcaea63ac0cfec1b79571321507c
LongNV2004 <longnguyen01042004@gmail.com>
2025-10-17 21:00:27 +0700

    Student Join Class

---
+++ Models/Domain/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace POETWeb.Models.Domain
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int ClassId { get; set; }
+
+        [ForeignKey(nameof(ClassId))]
+        public Classroom? Classroom { get; set; }
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [StringLength(30)]
+        public string RoleInClass { get; set; } = "Student";
+
+        public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+    }
+}
