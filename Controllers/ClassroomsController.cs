448ce4b2292636a7e23698c7f24dd1f5b57cae55
LongNV2004 <longnguyen01042004@gmail.com>
2025-10-21 15:12:46 +0700

    Student List of Students

--- Controllers/ClassroomsController.cs
+++ Controllers/ClassroomsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using POETWeb.Data;
 using POETWeb.Models;
@@ -229,6 +230,45 @@ namespace POETWeb.Controllers
             ViewBag.ClassId = classId;
             ViewBag.ClassName = cls.Name;
             return View();
+        }
+
+        //Danh sách học viên cho STUDENT xemm
+        [Authorize(Roles = "Student")]
+        public async Task<IActionResult> RosterStudent(int id)
+        {
+            var me = await _userManager.GetUserAsync(User);
+
+            bool joined = await _db.Enrollments.AnyAsync(e => e.ClassId == id && e.UserId == me!.Id);
+            if (!joined) return Forbid();
+
+            var cls = await _db.Classrooms.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+            if (cls == null) return NotFound();
+
+            var q = from e in _db.Enrollments
+                    join u in _db.Users on e.UserId equals u.Id
+                    where e.ClassId == id
+                    orderby u.FullName
+                    select new
+                    {
+                        u.FullName,
+                        u.Email,
+                        u.PhoneNumber,
+                        u.AccountCode,
+                        u.AvatarUrl,
+                        e.JoinedAt
+                    };
+
+            var list = await q.AsNoTracking().ToListAsync();
+            ViewBag.ClassId = cls.Id;
+            ViewBag.ClassName = cls.Name;
+            ViewBag.ClassCode = cls.ClassCode;
+            return View(list);
+        }
+        // helper
+        private async Task EnsureOwnerAsync(string teacherId)
+        {
+            var me = await _userManager.GetUserAsync(User);
+            if (me!.Id != teacherId) throw new UnauthorizedAccessException("Not your class.");
         }
     }
 }
