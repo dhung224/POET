0f14f7fb0ad19dfb6ce412380752b8af5f920534
hunghoclopa99999 <hunghoclopa99999@gmail.com>
2025-11-01 01:18:05 +0700

    fix bug

--- Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -1,5 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
 using System;
@@ -24,11 +23,6 @@ namespace POETWeb.Areas.Identity.Pages.Account
             _userManager = userManager;
             _signInManager = signInManager;
         }
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
 
